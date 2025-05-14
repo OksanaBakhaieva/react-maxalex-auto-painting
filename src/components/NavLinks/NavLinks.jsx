5ccec1dc713980e1c23eb6fe2bee065806b9e7c5
OksanaBakhaieva <obahaeva@gmail.com>
2025-05-14 11:30:27 +0100

    refactor: finalise scrolling and anchor navigation across components and pages

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import css from './NavLinks.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 function NavLinks({ text, href, variant}) {
     const NavLinksClasses = clsx(css.nav_link,
@@ -10,13 +10,38 @@ function NavLinks({ text, href, variant}) {
         }
     );
 
-    const isInternal = href.startsWith("/");
+    const navigate = useNavigate();
+    const location = useLocation();
 
-    return isInternal ? (
-            <Link className={NavLinksClasses} to={href}>{text}</Link>
-    ) : (
-            <a className={NavLinksClasses} href={href}>{text}</a>
-    )
+    const handleAnchorClick = (e) => {
+        e.preventDefault();
+        const [path, hash] = href.split('#');
+        if (location.pathname !== `/${path}` && path) { //переход на нужную страничку
+            navigate(`/${path}`, {    // путь,куда я хочу отправить
+                state: {                //состояние маршрута, доступно на целевой странице через useLocation()
+                    scrollTo: hash
+                }
+            });
+        } else if (hash) {     //eсли мы уже на нужной странице, то просто скроллим до указанной в hash секции
+            const section = document.getElementById(hash);
+            if (section) {
+                section.scrollIntoView({ behavior: 'smooth' });
+            }
+        } else {
+            navigate(path);     //для случаев, когда наши линки просто ведут на страничку
+        }
+    }
+
+    const isInternal = href.startsWith("/");
+    const isAnchor = href.startsWith('#');
+
+    if (isInternal) {
+        return <Link className={NavLinksClasses} to={href}>{text}</Link>
+    } else if (href.includes('#')) {
+        return <a className={NavLinksClasses} href={href} onClick={handleAnchorClick}>{text}</a>
+    } else {
+        return <a className={NavLinksClasses} href={href}>{text}</a>
+    }
 }
 
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,23 @@
+import { useLocation } from 'react-router-dom';
 import About from '../../components/About/About';
 import FAQ from '../../components/FAQ/FAQ';
 import Hero from '../../components/Hero/Hero';
 import Reviews from '../../components/Reviews/Reviews';
 import css from './HomePage.module.css';
+import { useEffect } from 'react';
 
 function HomePage() {
+    const location = useLocation();
+
+    useEffect(() => {   // используем т.к. нам нужно проскроллить к секции после navigate
+        if (location.state?.scrollTo) {
+            const section = document.getElementById(location.state.scrollTo);
+            if (section) {
+                section.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }, [location]);
+    
     return (
         <div className="container" id="home">
             <Hero />
